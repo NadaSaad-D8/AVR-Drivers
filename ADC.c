65854cf4377bbed99a34deb548b51c214fd84261
NadaSaad <70064017+NadaSaad-D8@users.noreply.github.com>
2020-09-23 00:32:40 +0200

    Add files via upload

---
+++ ADC.c
@@ -0,0 +1,49 @@
+#include "ADC.h"
+
+/*******************************************************************************
+ *                          Global Variables                                   *
+ *******************************************************************************/
+
+volatile uint16 g_adcResult = 0;
+/*******************************************************************************
+ *                          ISR's Definitions                                  *
+ *******************************************************************************/
+
+ISR(ADC_vect)
+{
+	/* Read ADC Data after conversion complete */
+	g_adcResult = ADC;
+}
+/*****************************************************************************
+ * 
+ * Description : Function to Initiliaze The ADC 
+ * ***************************************************************************/
+
+void ADCInit(const ADCConfigType* config_ptr)
+{
+  /*choose voltage */
+  ADMUX = ((ADMUX & 0xCF) | ((config_ptr -> RefrenceVoltage) <<6) );
+  /*choose the ADCResult Left or Right Adjusted */
+  ADMUX =((config_ptr -> ResultAdjust ) << 5) ;
+  /*Enable ADC */
+   ADCSRA |= (1<<7);
+  /*choose Clock Note : ADC must operate in range 50-200Khz */
+   ADCSRA = (( ADCSRA & 0xF8) |  (config_ptr -> Prescaler )  ) ;
+   #ifdef INTERRUPT 
+   ADCSRA |= (1<<ADIE);
+   #endif
+}
+/*****************************************************************************
+ * 
+ * Description : Read the channel of the ADC and Start Conversion 
+ * ***************************************************************************/
+void ADCReadChannel (const ADCConfigType* config_ptr)
+{
+    /*choose ADC channel */
+     ADMUX = ( (ADMUX & 0xE0) | (config_ptr ->   channel )) ; 
+   /* start conversion write '1' to ADSC */
+     SET_BIT(ADCSRA,ADSC);  
+     #ifndef INTERRUPT 
+     g_adcResult = ADC ;
+     #endif  
+}
---
+++ ADC.h
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ * Module :  LCD 
+ * File Name : ADC.h
+ * Description : header file for ADC driver 
+ * Author : Nada Mohamed 
+ * ***************************************************************************/
+ #include "micro_config.h"
+ #include "std_types.h"
+ #include "BITS_CONFIGURATION.h"
+ #ifndef ADC_H 
+ #define ADC_H
+ /****************************************************************************
+                    Types Declerations 
+ *****************************************************************************/
+typedef enum {AREF,AVCC_CAP,RESERVED,INTERNAL_REFRENCEVOLT }ADCRefVolt;
+typedef enum {ADC0,ADC1,ADC2,ADC3,ADC4,ADC5,ADC6,ADC7}ADCChannel;
+typedef enum {RIGHT,LEFT}ADCResultAdjust;
+typedef enum {CLK_2=1,CLK_4,CLK_8,CLK_16,CLK_32,CLK_64_CLK_128}ADCPrescaler;
+typedef struct {
+ADCRefVolt         RefrenceVoltage ;
+ADCChannel         channel         ;
+ADCResultAdjust    ResultAdjust    ;
+ADCPrescaler       Prescaler       ;
+}ADCConfigType ;
+
+#define INTERRUPT 
+ /*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/
+void ADCInit(const ADCConfigType* config_ptr);
+void ADCReadChannel (const ADCConfigType* config_ptr);
+
+ #endif
